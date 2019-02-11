400a14c03c10f07f3e4cfedd1df963d9263e98c8
Hannah <hannah_chow@brown.edu>
2019-02-11 01:22:49 -0500

    From last

---
+++ src/client/views/nodes/AnnotationView.tsx
@@ -0,0 +1,12 @@
+import React = require('react')
+import { CollectionViewProps } from '../collections/CollectionViewBase';
+
+export class AnnotationView extends React.Component<CollectionViewProps> {
+
+    render() {
+        return (
+            <></>
+        );
+    }
+
+}
