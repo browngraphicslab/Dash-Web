74617fa46353d82c7618af76e6878003fba68980
madelinegr <laura_wilson@brown.edu>
2019-02-28 00:21:15 -0500

    deleted some unused imports

--- src/client/views/nodes/KeyValuePair.tsx
+++ src/client/views/nodes/KeyValuePair.tsx
@@ -1,25 +1,12 @@
-import Lightbox from 'react-image-lightbox';
 import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 import "./KeyValuePane.scss";
 import React = require("react")
 import { FieldViewProps, FieldView } from './FieldView';
-import { FieldWaiting, Opt, Field } from '../../../fields/Field';
+import { Opt, Field } from '../../../fields/Field';
 import { observer } from "mobx-react"
-import { observable, action, IReactionDisposer, reaction, ObservableMap } from 'mobx';
-import { KeyStore } from '../../../fields/KeyStore';
-import { RichTextField } from "../../../fields/RichTextField";
-import { element } from 'prop-types';
-import { props } from 'bluebird';
-import { EditorView } from 'prosemirror-view';
-import { Transaction, EditorState } from 'prosemirror-state';
-import { schema } from 'prosemirror-schema-basic';
-import { keymap } from 'prosemirror-keymap';
-import { undo, redo } from 'prosemirror-history';
-import { baseKeymap } from 'prosemirror-commands';
-import { KVPField } from '../../../fields/KVPField';
+import { observable, action } from 'mobx';
 import { Document } from '../../../fields/Document';
 import { Key } from '../../../fields/Key';
-import { JSXElement } from 'babel-types';
 import { Server } from "../../Server"
 
 // Represents one row in a key value plane
