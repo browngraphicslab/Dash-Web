345c445474292fe0bf56ff1a36f5a9a4b8411e95
yipstanley <stanley_yip@brown.edu>
2019-03-17 19:35:25 -0400

    ah

--- src/mobile/ImageUpload.tsx
+++ src/mobile/ImageUpload.tsx
@@ -35,7 +35,7 @@ const onFileLoad = (file: any) => {
 
 ReactDOM.render((
     <div className="imgupload_cont">
-        <button className="button_file" onTouchStart={onPointerDown}> Open Image </button>
+        {/* <button className="button_file" onTouchStart={onPointerDown}> Open Image </button> */}
         <input type="file" accept="image/*" onChange={onFileLoad} className="input_file" id="input_image_file"></input>
         <img id="img_preview" src=""></img>
     </div>),
