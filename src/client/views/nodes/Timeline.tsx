1d7a74172bf91ddc58a68b4a2f6edf1fb5cff99d
Andrew Kim <andrewdkim@users.noreply.github.com>
2019-04-06 16:12:49 -0400

    timeline update

--- src/client/views/nodes/Timeline.tsx
+++ src/client/views/nodes/Timeline.tsx
@@ -1,5 +1,18 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
+import {observer} from "mobx-react"
 import {observable} from "mobx"
+import { TimelineField } from "../../../fields/TimelineField";
 
 
+
+@observer
+export class Timeline extends React.Component<TimelineField>{
+
+    render(){
+        return(
+        <div>
+            <h1 style={{left: `${window.pageXOffset} - 0px`, top: "100px", position:"absolute"}}> hi</h1>
+        </div>)
+    }
+}
