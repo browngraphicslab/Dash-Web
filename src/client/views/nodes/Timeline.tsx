d69334e5e70b9b3e0346231b37304ddaa989f86c
Andrew Kim <andrewdkim@users.noreply.github.com>
2019-04-08 17:52:33 -0400

    graphics change

--- src/client/views/nodes/Timeline.tsx
+++ src/client/views/nodes/Timeline.tsx
@@ -10,16 +10,24 @@ import "./Timeline.scss"
 @observer
 export class Timeline extends React.Component<TimelineField>{
 
+    private _isRecording = false; 
+    onRecord = (e:React.MouseEvent) => {
+        this._isRecording = true; 
+    }
+
+    onStop = (e:React.MouseEvent) => {
+        this._isRecording = false; 
+    }
+
     render() {
         return (
             <div>
                 <div className="timeline-container"> 
                     <div className="timeline">
-                       
 
                     </div> 
-                    <button>Record</button>
-                    <button> Stop </button>
+                    <button onClick = {this.onRecord}>Record</button>
+                    <button onClick = {this.onStop}> Stop </button>
                 </div>
             </div>
         )
