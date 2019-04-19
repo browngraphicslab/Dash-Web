c8dcb2a6f93374c426f8b10292d28ca4a79b7ec0
bob <bcz@cs.brown.edu>
2019-04-19 16:57:44 -0400

    moved icon into documentview until it can find a better home.

--- src/client/views/nodes/IconBox.tsx
+++ src/client/views/nodes/IconBox.tsx
@@ -36,54 +36,9 @@ export class IconBox extends React.Component<FieldViewProps> {
         return <FontAwesomeIcon icon={button} className="documentView-minimizedIcon" />
     }
 
-    animateTransition(icon: number[], targ: number[], width: number, height: number, stime: number, target: Document, maximizing: boolean) {
-        setTimeout(() => {
-            let now = Date.now();
-            let progress = Math.min(1, (now - stime) / 200);
-            let pval = maximizing ?
-                [icon[0] + (targ[0] - icon[0]) * progress, icon[1] + (targ[1] - icon[1]) * progress] :
-                [targ[0] + (icon[0] - targ[0]) * progress, targ[1] + (icon[1] - targ[1]) * progress];
-            target.SetNumber(KeyStore.Width, maximizing ? 25 + (width - 25) * progress : width + (25 - width) * progress);
-            target.SetNumber(KeyStore.Height, maximizing ? 25 + (height - 25) * progress : height + (25 - height) * progress);
-            target.SetNumber(KeyStore.X, pval[0]);
-            target.SetNumber(KeyStore.Y, pval[1]);
-            if (now < stime + 200) {
-                this.animateTransition(icon, targ, width, height, stime, target, maximizing);
-            }
-            else {
-                if (!maximizing) {
-                    target.SetBoolean(KeyStore.IsMinimized, true);
-                    target.SetNumber(KeyStore.X, targ[0]);
-                    target.SetNumber(KeyStore.Y, targ[1]);
-                    target.SetNumber(KeyStore.Width, width);
-                    target.SetNumber(KeyStore.Height, height);
-                }
-                this._completed = true;
-            }
-        },
-            2);
-    }
-
-    _completed = true;
-
-    @action
-    public toggleMinimize = (): void => {
-        SelectionManager.DeselectAll();
-        if (this.maximized instanceof Document && this._completed) {
-            this._completed = false;
-            let minimized = this.maximized.GetBoolean(KeyStore.IsMinimized, false);
-            this.maximized.SetBoolean(KeyStore.IsMinimized, false);
-            this.animateTransition(
-                [this.props.Document.GetNumber(KeyStore.X, 0), this.props.Document.GetNumber(KeyStore.Y, 0)],
-                [this.maximized.GetNumber(KeyStore.X, 0), this.maximized.GetNumber(KeyStore.Y, 0)],
-                this.maximized.GetNumber(KeyStore.Width, 0), this.maximized.GetNumber(KeyStore.Width, 0),
-                Date.now(), this.maximized, minimized);
-        }
-    }
-
     render() {
         return (
-            <div className="iconBox-container" onClick={this.toggleMinimize} >
+            <div className="iconBox-container">
                 {this.minimizedIcon}
             </div>);
     }
