b18aec5d4b07bbc859a5b1b2d22ca3bd92ca53cd
Tyler Schicke <tyler_schicke@brown.edu>
2019-05-09 20:30:40 -0400

    Added try catches to search to not throw errors on the server

--- src/server/Search.ts
+++ src/server/Search.ts
@@ -7,31 +7,39 @@ export class Search {
     private url = 'http://localhost:8983/solr/';
 
     public async updateDocument(document: any) {
-        return rp.post(this.url + "dash/update", {
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify([document])
-        });
+        try {
+            return rp.post(this.url + "dash/update", {
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify([document])
+            });
+        } catch { }
     }
 
     public async search(query: string) {
-        const searchResults = JSON.parse(await rp.get(this.url + "dash/select", {
-            qs: {
-                q: query
-            }
-        }));
-        const fields = searchResults.response.docs;
-        const ids = fields.map((field: any) => field.id);
-        return ids;
+        try {
+            const searchResults = JSON.parse(await rp.get(this.url + "dash/select", {
+                qs: {
+                    q: query
+                }
+            }));
+            const fields = searchResults.response.docs;
+            const ids = fields.map((field: any) => field.id);
+            return ids;
+        } catch {
+            return [];
+        }
     }
 
     public async clear() {
-        return rp.post(this.url + "dash/update", {
-            body: {
-                delete: {
-                    query: "*:*"
-                }
-            },
-            json: true
-        });
+        try {
+            return rp.post(this.url + "dash/update", {
+                body: {
+                    delete: {
+                        query: "*:*"
+                    }
+                },
+                json: true
+            });
+        } catch { }
     }
 }
