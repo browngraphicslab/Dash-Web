b12f2f08552331a3a86e49f54d013f69180a061a
Mohammad Amoush <muhammedamoush@gmail.com>
2019-06-12 17:20:41 -0400

    working on hovering, sorting more dynamic

--- src/client/views/collections/CollectionTimelineView.tsx
+++ src/client/views/collections/CollectionTimelineView.tsx
@@ -1,68 +1,236 @@
 import React = require("react");
-import { action, computed, IReactionDisposer, reaction, observable } from "mobx";
-import { observer} from "mobx-react";
-import { Doc, HeightSym, WidthSym } from "../../../new_fields/Doc";
+import { action, computed, IReactionDisposer, reaction, observable, untracked } from "mobx";
+import { observer } from "mobx-react";
+import { Doc, HeightSym, WidthSym, DocListCast, Opt } from "../../../new_fields/Doc";
 import { Id } from "../../../new_fields/FieldSymbols";
-import { BoolCast, NumCast, StrCast } from "../../../new_fields/Types";
-import { emptyFunction, returnOne, Utils } from "../../../Utils";
+import { BoolCast, NumCast, StrCast, Cast, FieldValue, } from "../../../new_fields/Types";
+import { emptyFunction, returnOne, Utils, returnFalse } from "../../../Utils";
 import { SelectionManager } from "../../util/SelectionManager";
 import { undoBatch } from "../../util/UndoManager";
-import { DocumentView } from "../nodes/DocumentView";
+import { DocumentView, DocumentViewProps } from "../nodes/DocumentView";
 import { CollectionSchemaPreview } from "./CollectionSchemaView";
 import "./CollectionTimelineView.scss";
 import { CollectionSubView } from "./CollectionSubView";
+import { anchorPoints, Flyout } from "../DocumentDecorations";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { DateTimeStep } from "../../northstar/model/idea/idea";
+import { date } from "serializr";
+import { DateField } from "../../../new_fields/DateField";
+import { List } from "../../../new_fields/List";
+import { DocumentContentsView } from "../nodes/DocumentContentsView";
+import { Transform } from "../../util/Transform";
+import { CollectionView } from "./CollectionView";
+import { CollectionPDFView } from "./CollectionPDFView";
+import { CollectionVideoView } from "./CollectionVideoView";
+import { VideoBox } from "../nodes/VideoBox";
+import { faFilePowerpoint } from "@fortawesome/free-solid-svg-icons";
+
+
+export interface FieldViewProps {
+    fieldKey: string;
+    ContainingCollectionView: Opt<CollectionView | CollectionPDFView | CollectionVideoView>;
+    Document: Doc;
+    isSelected: () => boolean;
+    select: (isCtrlPressed: boolean) => void;
+    isTopMost: boolean;
+    selectOnLoad: boolean;
+    addDocument?: (document: Doc, allowDuplicates?: boolean) => boolean;
+    addDocTab: (document: Doc, where: string) => void;
+    removeDocument?: (document: Doc) => boolean;
+    moveDocument?: (document: Doc, targetCollection: Doc, addDocument: (document: Doc) => boolean) => boolean;
+    ScreenToLocalTransform: () => Transform;
+    active: () => boolean;
+    whenActiveChanged: (isActive: boolean) => void;
+    focus: (doc: Doc) => void;
+    PanelWidth: () => number;
+    PanelHeight: () => number;
+    setVideoBox?: (player: VideoBox) => void;
+}
+
+
+@observer
+class KeyToggle extends React.Component<{ keyName: string, toggle: (key: string) => void }> {
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div key={this.props.keyName}>
+                <input type="radio" name="dude" onChange={() => this.props.toggle(this.props.keyName)} />
+                {this.props.keyName}
+            </div>
+        );
+    }
+}
 
 @observer
 export class CollectionTimelineView extends CollectionSubView(doc => doc) {
 
-    buttonloop(){
-      let buttons = [];
-      let arr = [];
-      //Building the array is kinda weird because I reverse engineered something from another class.
-      this.childDocs.filter(d => !d.isMinimized).map((d, i) => {
-          arr.push(StrCast(d.title))
-      });
-      arr= this.selectionSort(arr);
-      var len = arr.length;
+    @computed get previewWidth() { return () => NumCast(this.props.Document.schemaPreviewWidth); }
 
-      for (let i=0;i<arr.length;i++){
-        buttons.push(
-            <button
-style={{position:"absolute",
-          top: "50%", left:(i*100/len)+ "%", width:(5/(2*Math.log2((len/10)+1)))+ "%"}}>{arr[i]}</button>)
-      }
-      return buttons;
+    @observable
+    public sortstate: String = "date";
+
+    @observable
+    buttonloop() {
+        let buttons = [];
+        let arr: Doc[] = [];
+        //Building the array is kinda weird because I reverse engineered something from another class.
+        this.childDocs.filter(d => !d.isMinimized).map((d, i) => {
+            arr.push(d);
+        });
+        if (this.sortstate === "creationDate") {
+            arr.sort(this.sortdate);
+        }
+        if (this.sortstate === "title") {
+            arr.sort(this.sorttitle);
+        }
+
+        if (this.sortstate === "x") {
+            arr.sort(this.sortx);
+        }
+
+        if (this.sortstate === "y") {
+            arr.sort(this.sorty);
+        }
+        if (this.sortstate === "height") {
+            arr.sort(this.sortheight);
+        }
+
+
+
+        var len = arr.length;
+
+        let preview: String = "";
+        let returnHundred = () => 100;
+        let hover = false;
+
+        for (let i = 0; i < arr.length; i++) {
+            let preview = <DocumentContentsView Document={arr[i]}
+                addDocument={undefined}
+                addDocTab={this.props.addDocTab}
+                removeDocument={undefined}
+                ScreenToLocalTransform={Transform.Identity}
+                ContentScaling={returnOne}
+                PanelWidth={returnHundred}
+                PanelHeight={returnHundred}
+                isTopMost={true}
+                selectOnLoad={false}
+                focus={emptyFunction}
+                isSelected={this.props.isSelected}
+                select={returnFalse}
+                layoutKey={"layout"}
+                ContainingCollectionView={this.props.ContainingCollectionView}
+                parentActive={this.props.active}
+                whenActiveChanged={this.props.whenActiveChanged}
+                bringToFront={emptyFunction} />
+            buttons.push(
+                <div>
+                    <button onMouseOver={function flipp() { hover = true; }}
+                        onMouseLeave={function flipp() { hover = false; }}
+                        style={{
+                            position: "absolute",
+                            top: "50%", left: (i * 100 / len) + "%", width: (5 / (2 * Math.log2((len / 10) + 1))) + "%"
+                        }}>{arr[i].title}</button>
+
+                </div>)
+        }
+        return buttons;
+    }
+
+    sortx(a: Doc, b: Doc) {
+        return (a.x - b.x);
+    }
+
+    sorty(a: Doc, b: Doc) {
+        return (a.y - b.y);
+    }
+
+    sortheight(a: Doc, b: Doc) {
+        return (a.height - b.height);
+    }
+
+    sorttitle(a: Doc, b: Doc) {
+        return a.title.localeCompare(b.title);
+    }
+
+    sortdate(a: Doc, b: Doc) {
+        var adate: DateField = a.creationDate;
+        var bdate: DateField = b.creationDate;
+        return new Date(bdate.date) - new Date(adate.date);
     }
 
 
-selectionSort(arr){
-  var minIdx, temp,
-      len = arr.length;
-  for(var i = 0; i < len; i++){
-    minIdx = i;
-    for(var  j = i+1; j<len; j++){
-       if(arr[j]<arr[minIdx]){
-          minIdx = j;
-       }
+    get tableOptionsPanel() {
+        const docs = DocListCast(this.props.Document[this.props.fieldKey]);
+        let keys: { [key: string]: boolean } = {};
+        untracked(() => docs.map(doc => Doc.GetAllPrototypes(doc).map(proto => Object.keys(proto).forEach(key => keys[key] = false))));
+
+        return !this.props.active() ? (null) :
+            (<Flyout
+                anchorPoint={anchorPoints.RIGHT_TOP}
+                content={<div>
+                    <div id="schema-options-header"><h5><b>Options</b></h5></div>
+                    <div id="options-flyout-div">
+                        {Array.from(Object.keys(keys)).map(item =>
+                            (<KeyToggle key={item} keyName={item} toggle={this.toggleKey} />))}
+
+                    </div>
+                </div>
+                }>
+                <button id="schemaOptionsMenuBtn" ><FontAwesomeIcon style={{ color: "white" }} icon="cog" size="sm" /></button>
+            </Flyout>);
     }
-    temp = arr[i];
-    arr[i] = arr[minIdx];
-    arr[minIdx] = temp;
-  }
-  return arr;
-}
+    @action
+    toggleKey = (key: string) => {
+        this.sortstate = key;
+        console.log(this.sortstate);
+        console.log(this.sortstate);
+    }
+
+
+    @action
+    show(hover: boolean, document: Doc) {
+        console.log(document)
+        let returnHundred = () => 100;
+        if (hover) {
+            return (
+                <DocumentContentsView Document={document}
+                    addDocument={undefined}
+                    addDocTab={this.props.addDocTab}
+                    removeDocument={undefined}
+                    ScreenToLocalTransform={Transform.Identity}
+                    ContentScaling={returnOne}
+                    PanelWidth={returnHundred}
+                    PanelHeight={returnHundred}
+                    isTopMost={true}
+                    selectOnLoad={false}
+                    focus={emptyFunction}
+                    isSelected={this.props.isSelected}
+                    select={returnFalse}
+                    layoutKey={"layout"}
+                    ContainingCollectionView={this.props.ContainingCollectionView}
+                    parentActive={this.props.active}
+                    whenActiveChanged={this.props.whenActiveChanged}
+                    bringToFront={emptyFunction} />
+            );
+        }
+    }
+
+
 
 
     render() {
-        let thing=this.singleColumnChildren;
         return (
-          <div className="collectionTimelineView" style={{ height: "100%" }}
-           onWheel={(e: React.WheelEvent) => e.stopPropagation()}>
-              <hr style={{top:"50%", display:"block", width:"100%", border:"10", position:"absolute"}}/>
-
+            <div className="collectionTimelineView" style={{ height: "100%" }}
+                onWheel={(e: React.WheelEvent) => e.stopPropagation()}>
+                <hr style={{ top: "50%", display: "block", width: "100%", border: "10", position: "absolute" }} />
+                {this.tableOptionsPanel}
                 {this.buttonloop()}
 
-          </div>
+
+            </div>
         );
     }
 }
