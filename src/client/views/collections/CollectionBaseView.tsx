0d5e2537520ca1e6a6b52f4d0f03aa2bcfc6c5c6
Sam Wilkins <35748010+samwilkins333@users.noreply.github.com>
2019-06-26 12:30:16 -0400

    cleanup

--- src/client/views/collections/CollectionBaseView.tsx
+++ src/client/views/collections/CollectionBaseView.tsx
@@ -146,9 +146,6 @@ export class CollectionBaseView extends React.Component<CollectionViewProps> {
             }
         });
 
-        //initial
-        //
-
         if (index !== -1) {
             value.splice(index, 1);
 
