b5740401b027c372edb5bbf465f91abfda663705
monikahedman <monika_hedman@brown.edu>
2019-07-14 15:00:47 -0400

    small ui changes done

--- src/client/views/search/SearchBox.tsx
+++ src/client/views/search/SearchBox.tsx
@@ -170,7 +170,7 @@ export class SearchBox extends React.Component {
         return (
             <div className="searchBox-container">
                 <div className="searchBox-bar">
-                    <span className="searchBox-barChild searchBox-collection" onPointerDown={SetupDrag(this.collectionRef, this.startDragCollection)} ref={this.collectionRef}>
+                    <span className="searchBox-barChild searchBox-collection" onPointerDown={SetupDrag(this.collectionRef, this.startDragCollection)} ref={this.collectionRef} title="Drag Results as Collection">
                         <FontAwesomeIcon icon="object-group" size="lg" />
                     </span>
                     <input value={this._searchString} onChange={this.onChange} type="text" placeholder="Search..."
