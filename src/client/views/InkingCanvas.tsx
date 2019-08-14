658cd061fd1927148c3542e4f82f06c6805f98db
Bob Zeleznik <zzzman@gmail.com>
2019-08-14 17:58:42 -0400

    removed log msgs.

--- src/client/views/InkingCanvas.tsx
+++ src/client/views/InkingCanvas.tsx
@@ -68,7 +68,6 @@ export class InkingCanvas extends React.Component<InkCanvasProps> {
     @action
     onPointerDown = (e: React.PointerEvent): void => {
         if (e.button !== 0 || e.altKey || e.ctrlKey || InkingControl.Instance.selectedTool === InkTool.None) {
-            console.log("RETURNING!");
             return;
         }
 
@@ -78,8 +77,6 @@ export class InkingCanvas extends React.Component<InkCanvasProps> {
         e.preventDefault();
 
         this.previousState = new Map(this.inkData);
-
-        console.log("POINTER DOWN");
 
         if (InkingControl.Instance.selectedTool !== InkTool.Eraser) {
             // start the new line, saves a uuid to represent the field of the stroke
