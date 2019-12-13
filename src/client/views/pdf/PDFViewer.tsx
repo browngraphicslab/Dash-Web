a2d2737fa4ec1a57f5288ed72eb44a2be4588ee8
Sam Wilkins <samwilkins333@gmail.com>
2019-12-13 14:25:35 -0500

    pdf path parsing fix in pdfviewer

--- src/client/views/pdf/PDFViewer.tsx
+++ src/client/views/pdf/PDFViewer.tsx
@@ -125,7 +125,7 @@ export class PDFViewer extends DocAnnotatableComponent<IViewerProps, PdfDocument
         !this.props.Document.lockedTransform && (this.props.Document.lockedTransform = true);
         // change the address to be the file address of the PNG version of each page
         // file address of the pdf
-        const path = Utils.prepend(`/thumbnail${this.props.url.substring("files/pdfs/".length, this.props.url.length - ".pdf".length)}-${(this.Document.curPage || 1)}.png`);
+        const path = Utils.prepend(`/files/pdf_thumbnails${this.props.url.substring("files/pdfs/".length, this.props.url.length - ".pdf".length)}-${(this.Document.curPage || 1)}.png`);
         this._coverPath = JSON.parse(await rp.get(path));
         runInAction(() => this._showWaiting = this._showCover = true);
         this.props.startupLive && this.setupPdfJsViewer();
