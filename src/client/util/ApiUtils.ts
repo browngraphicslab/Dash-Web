4fc9539208f1814f312879a9cab1afa2b1258a69
Tyler Schicke <tschicke@gmail.com>
2019-12-24 13:52:33 -0500

    Added update functionality for parsed tables
    Tables now store a primaryKey field that is used to identify rows when
    an update is done. These rows are updated with the new values in the
    table instead of creating a new document for the row. If there is a new
    row in the table then a new Doc will be created and added to the table.
    
    This should allow the parse function to be run once when the api/table
    is set up, and then the update function to be run periodically to update
    the table.
    
    Currently deleting row in the Dash table that aren't in the api's table
    isn't done. I think along with adding an option to do that, I want to
    add an option to just mark those documents as being deleted from the
    table, so that they can be filtered, searched for, etc., without the
    user unknowingly having documents disappear from the table that they
    wanted to keep.

--- src/client/util/ApiUtils.ts
+++ src/client/util/ApiUtils.ts
@@ -1,8 +1,11 @@
 import * as rp from 'request-promise';
-import { Opt, Doc } from '../../new_fields/Doc';
+import { Opt, Doc, DocListCastAsync } from '../../new_fields/Doc';
 import { Docs } from '../documents/Documents';
 import { SchemaHeaderField } from '../../new_fields/SchemaHeaderField';
 import { Utils } from '../../Utils';
+import { List } from '../../new_fields/List';
+import { listSpec } from '../../new_fields/Schema';
+import { Cast } from '../../new_fields/Types';
 
 export namespace ApiUtils {
     export async function fetchHtml(url: string): Promise<Opt<Document>> {
@@ -20,23 +23,29 @@ export namespace ApiUtils {
     export function getTableHeaders(table: HTMLTableElement): string[] {
         const headerCells = table.rows[0].cells;
         const headers: string[] = [];
+        let numUnnamed = 0;
         for (let i = 0; i < headerCells.length; i++) {
-            const text = headerCells[i].textContent;
-            if (text) {
-                headers.push(text);
-            }
+            const name = headerCells[i].textContent ?? `column_${numUnnamed++}`;
+            headers.push(name);
         }
         return headers;
     }
 
-    export function parseTable(table: HTMLTableElement, columns?: string[], rowFunc?: (row: { [column: string]: string }) => boolean): Doc {
+    export interface ParseTableOptions {
+        primaryKey: string;
+        columns?: string[];
+        //rowFunc?: (row: { [column: string]: string }) => boolean;
+    }
+
+    export function parseTable(table: HTMLTableElement, options: ParseTableOptions): Doc {
+        const { primaryKey, columns } = options;
         const headerRow = table.rows[0].cells;
         const includedColumns: { index: number, name: string }[] = [];
         const columnSet = new Set(columns);
         let numUnnamed = 0;
         for (let i = 0; i < headerRow.length; i++) {
             const name = headerRow[i].textContent ?? `column_${numUnnamed++}`;
-            if (columns && columnSet.has(name)) {
+            if (!columns || columnSet.has(name)) {
                 includedColumns.push({ name, index: i });
             }
         }
@@ -49,13 +58,67 @@ export namespace ApiUtils {
                 const cell = row.cells[column.index];
                 rowObj[column.name] = cell.textContent ?? "";
             }
-            if (rowFunc?.(rowObj) ?? true) {
-                const row = new Doc();
-                Doc.assign(row, rowObj);
-                docs.push(row);
-            }
+            const rowDoc = new Doc();
+            Doc.assign(rowDoc, rowObj);
+            docs.push(rowDoc);
         }
-        return Docs.Create.SchemaDocument(includedColumns.map(col => new SchemaHeaderField(col.name)), docs, {});
+        const collection = Docs.Create.SchemaDocument(includedColumns.map(col => new SchemaHeaderField(col.name)), docs, {});
+        collection.primaryKey = primaryKey;
+        if (columns) {
+            collection.activeColumns = new List(columns);
+        }
+        return collection;
     }
 
+    export async function updateTable(table: HTMLTableElement, doc: Doc): Promise<boolean> {
+        let docs = await DocListCastAsync(doc.data);
+        if (!docs) {
+            const list = new List<Doc>();
+            docs = list as Doc[];
+            doc.data = list;
+        }
+        const primaryKey = Cast(doc.primaryKey, "string");
+        const columns = Cast(doc.activeColumns, listSpec("string"));
+        if (!primaryKey) {
+            return false;
+        }
+        const docMap: { [key: string]: Doc } = {};
+        for (let i = 0; i < docs.length; i++) {
+            const doc = docs[i];
+            const key = Cast(doc[primaryKey], "string");
+            if (key) {
+                docMap[key] = doc;
+            }
+        }
+        const headerRow = table.rows[0].cells;
+        const includedColumns: { index: number, name: string }[] = [];
+        const columnSet = new Set(columns);
+        let numUnnamed = 0;
+        for (let i = 0; i < headerRow.length; i++) {
+            const name = headerRow[i].textContent ?? `column_${numUnnamed++}`;
+            if ((!columns || columnSet.has(name)) || name === primaryKey) {
+                includedColumns.push({ name, index: i });
+            }
+        }
+
+        for (let i = 1; i < table.rows.length; i++) {
+            const row = table.rows[i];
+            let rowObj: { [column: string]: string } = {};
+            for (const column of includedColumns) {
+                const cell = row.cells[column.index];
+                rowObj[column.name] = cell.textContent ?? "";
+            }
+            const key = rowObj[primaryKey];
+            if (!key) continue;
+            let rowDoc = docMap[key];
+            if (!rowDoc) {
+                rowDoc = new Doc;
+                Doc.assign(rowDoc, rowObj);
+                docs.push(rowDoc);
+            } else {
+                Doc.assign(rowDoc, rowObj);
+            }
+        }
+        return true;
+    }
 }
--- src/client/views/ApiTester.tsx
+++ src/client/views/ApiTester.tsx
@@ -17,7 +17,12 @@ export class ApiTester extends React.Component {
     selector: string = "";
 
     @observable
+    index?: number;
+
+    @observable
     columns?: { name: string, enabled: boolean }[];
+    @observable
+    primaryColumn?: string;
 
     @observable
     doc?: Doc;
@@ -34,6 +39,15 @@ export class ApiTester extends React.Component {
         this.columns = undefined;
     }
 
+    @action
+    onIndexChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const index = parseInt(e.currentTarget.value);
+        if (!isNaN(index)) {
+            this.index = index;
+            this.columns = undefined;
+        }
+    }
+
     submitCols = async () => {
         const page = await ApiUtils.fetchHtml(this.url);
         const table = page?.querySelector(this.selector);
@@ -42,19 +56,44 @@ export class ApiTester extends React.Component {
         }
         runInAction(() => {
             this.columns = ApiUtils.getTableHeaders(table).map(name => ({ name, enabled: true }));
+            this.primaryColumn = this.columns[0].name;
         });
     }
 
     @action
     submit = async () => {
+        if (!this.primaryColumn) {
+            return;
+        }
         const page = await ApiUtils.fetchHtml(this.url);
-        const table = page?.querySelector(this.selector);
+        const table = page?.querySelectorAll(this.selector)[this.index ?? 0];
         if (!(table instanceof HTMLTableElement)) {
             return;
         }
 
         runInAction(() => {
-            this.doc = ApiUtils.parseTable(table, this.columns?.filter(col => col.enabled).map(col => col.name));
+            this.doc = ApiUtils.parseTable(table, { columns: this.columns?.filter(col => col.enabled).map(col => col.name), primaryKey: this.primaryColumn });
+            this.columns = undefined;
+        });
+    }
+
+    @action
+    update = async () => {
+        if (!this.doc) {
+            return;
+        }
+        if (!this.primaryColumn) {
+            return;
+        }
+        const doc = this.doc;
+        const page = await ApiUtils.fetchHtml(this.url);
+        const table = page?.querySelectorAll(this.selector)[this.index ?? 0];
+        if (!(table instanceof HTMLTableElement)) {
+            return;
+        }
+
+        runInAction(() => {
+            ApiUtils.updateTable(table, doc);
             this.columns = undefined;
         });
     }
@@ -64,32 +103,43 @@ export class ApiTester extends React.Component {
             <div style={{ background: "gray" }}>
                 <div><input value={this.url} onChange={this.onUrlChanged}></input></div>
                 <div><input value={this.selector} onChange={this.onSelectorChanged}></input></div>
+                <div><input value={this.index} onChange={this.onIndexChanged}></input></div>
                 {this.columns ? this.columns.map(col => {
                     return (
-                        <label key={col.name} >
-                            <input checked={col.enabled} type="checkbox"
-                                onChange={action((e: React.ChangeEvent<HTMLInputElement>) => col.enabled = e.currentTarget.checked)} name={col.name} />
-                            {col.name}
-                        </label>
+                        <div key={col.name}>
+                            <label>
+                                <input checked={col.enabled} type="checkbox"
+                                    onChange={action((e: React.ChangeEvent<HTMLInputElement>) => col.enabled = e.currentTarget.checked)} name={col.name} />
+                                {col.name}
+                            </label>
+                            <label>
+                                Primary:
+                            <input checked={this.primaryColumn === col.name} type="checkbox"
+                                    onChange={action((e: React.ChangeEvent<HTMLInputElement>) => this.primaryColumn = col.name)} name={col.name} />
+                            </label>
+                        </div>
                     );
                 }) : null}
                 <div><button onClick={this.submitCols}>Get Columns</button></div>
                 <div><button onClick={this.submit}>Submit</button></div>
-                {this.doc ? <DocumentView Document={this.doc} ContainingCollectionDoc={undefined}
-                    ContainingCollectionView={undefined} LibraryPath={[]} ScreenToLocalTransform={() => Transform.Identity()} renderDepth={0}
-                    ruleProvider={undefined}
-                    PanelHeight={() => 500} PanelWidth={() => 500}
-                    ContentScaling={returnOne}
-                    focus={emptyFunction}
-                    parentActive={returnFalse}
-                    whenActiveChanged={emptyFunction}
-                    bringToFront={emptyFunction}
-                    addDocTab={returnFalse}
-                    pinToPres={emptyFunction}
-                    zoomToScale={emptyFunction}
-                    backgroundColor={() => undefined}
-                    getScale={returnOne}
-                /> : null}
+                <div><button onClick={this.update} disabled={this.doc === undefined}>Update</button></div>
+                <div>
+                    {this.doc ? <DocumentView Document={this.doc} ContainingCollectionDoc={undefined}
+                        ContainingCollectionView={undefined} LibraryPath={[]} ScreenToLocalTransform={() => Transform.Identity()} renderDepth={0}
+                        ruleProvider={undefined}
+                        PanelHeight={() => 500} PanelWidth={() => 500}
+                        ContentScaling={returnOne}
+                        focus={emptyFunction}
+                        parentActive={returnFalse}
+                        whenActiveChanged={emptyFunction}
+                        bringToFront={emptyFunction}
+                        addDocTab={returnFalse}
+                        pinToPres={emptyFunction}
+                        zoomToScale={emptyFunction}
+                        backgroundColor={() => undefined}
+                        getScale={returnOne}
+                    /> : null}
+                </div>
             </div>
         );
     }
