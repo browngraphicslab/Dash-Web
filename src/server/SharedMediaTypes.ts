3cce5982497564a0f5d69d0248ed07d76ec7bbe8
Sam Wilkins <samwilkins333@gmail.com>
2020-02-14 15:36:53 -0500

    more image formats

--- src/server/SharedMediaTypes.ts
+++ src/server/SharedMediaTypes.ts
@@ -2,7 +2,9 @@ export namespace AcceptibleMedia {
     export const gifs = [".gif"];
     export const pngs = [".png"];
     export const jpgs = [".jpg", ".jpeg"];
-    export const imageFormats = [...pngs, ...jpgs, ...gifs];
+    export const webps = [".webp"];
+    export const tiffs = [".tiff"];
+    export const imageFormats = [...pngs, ...jpgs, ...gifs, ...webps, ...tiffs];
     export const videoFormats = [".mov", ".mp4"];
     export const applicationFormats = [".pdf"];
 }
