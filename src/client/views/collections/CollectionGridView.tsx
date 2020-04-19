f20ee643c0df3d4f95d5cb300477af52c498d5b9
usodhi <61431818+usodhi@users.noreply.github.com>
2020-04-19 12:50:58 +0530

    created grid files and updated CollectionView

--- src/client/views/collections/CollectionGridView.tsx
+++ src/client/views/collections/CollectionGridView.tsx
@@ -13,17 +13,35 @@ import { CollectionSubView } from './CollectionSubView';
 import { List } from '../../../new_fields/List';
 import { returnZero } from '../../../Utils';
 
+import "../../../../node_modules/react-grid-layout/css/styles.css";
+import "../../../../node_modules/react-resizable/css/styles.css";
+import GridLayout from 'react-grid-layout';
+
+// import _ from "lodash";
+// import Responsive from '../../lib/ResponsiveReactGridLayout';
+// import WidthProvider from '../../lib/components/WidthProvider';
+// import type { CompactType, Layout } from '../../lib/utils';
+// const ResponsiveReactGridLayout = WidthProvider(Responsive);
+
+
 
 
 type GridSchema = makeInterface<[typeof documentSchema]>;
 const GridSchema = makeInterface(documentSchema);
 
 export class CollectionGridView extends CollectionSubView(GridSchema) {
-    render() {
+    render(): JSX.Element {
+        const layout = [
+            { i: 'a', x: 0, y: 0, w: 1, h: 2, static: true },
+            { i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+            { i: 'c', x: 4, y: 0, w: 1, h: 2 }
+        ];
         return (
-            <div>
-
-            </div>
+            <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+                <div key="a">a</div>
+                <div key="b">b</div>
+                <div key="c">c</div>
+            </GridLayout>
         );
     }
 }
