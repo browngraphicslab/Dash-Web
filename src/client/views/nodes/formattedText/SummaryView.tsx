42d101e829013d324e96eb4b2fbdd7c5d27625af
safaemerigh <59888326+safaemerigh@users.noreply.github.com>
2020-05-11 20:49:00 +0000

    SummaryView_2.0
    SummaryView with unknown issue on the fieldWrapper

--- src/client/views/nodes/formattedText/SummaryView.tsx
+++ src/client/views/nodes/formattedText/SummaryView.tsx
@@ -1,33 +1,101 @@
-import { TextSelection } from "prosemirror-state";
+import { IReactionDisposer, observable, computed, action } from "mobx";
 import { Fragment, Node, Slice } from "prosemirror-model";
+import { TextSelection } from "prosemirror-state";
 
+import * as ReactDOM from 'react-dom';
 import React = require("react");
+import { dom } from "@fortawesome/fontawesome-svg-core";
+import { observer } from "mobx-react";
 
-interface ISummaryView {
+export class SummaryView {
+
+    _fieldWrapper: HTMLDivElement; // container for label 
+
+    constructor(node: any, view: any, getPos: any) {
+
+        this._fieldWrapper = document.createElement("div");
+        this._fieldWrapper.style.fontWeight = "bold";
+        this._fieldWrapper.style.position = "relative";
+        this._fieldWrapper.style.display = "inline-block";
+        this._fieldWrapper.style.backgroundColor = "red";
+
+        const js = node.toJSON;
+        node.toJSON = function () {
+            return js.apply(this, arguments);
+        };
+
+        console.log("rendering new SummaryViewInternal")
+        ReactDOM.render(<SummaryViewInternal
+            view={view}
+            getPos={getPos}
+            node={node}
+
+        />, this._fieldWrapper);
+        (this as any).dom = this._fieldWrapper;
+    }
+
+    selectNode() { }
+    deselectNode() { }
+
+    destroy() {
+        ReactDOM.unmountComponentAtNode(this._fieldWrapper);
+    }
+
+}
+
+interface ISummaryViewInternal {
     node: any;
     view: any;
     getPos: any;
-    self: any;
 }
-export class SummaryView extends React.Component<ISummaryView> {
 
-    onPointerDown = (e: any) => {
+export class SummaryViewInternal extends React.Component<ISummaryViewInternal>{
+    _className: any;
+    _view: any;
+    _reactionDisposer: IReactionDisposer | undefined;
+
+    constructor(props: ISummaryViewInternal) {
+        super(props);
+
+        this._className = this.className(this.props.node.attrs.visibility);
+        this._view = this.props.view;
+
+        this.onPointerDownCollapsed = this.onPointerDownCollapsed.bind(this);
+        this.updateSummarizedText = this.updateSummarizedText.bind(this);
+
+    }
+
+    componentWillUnmount() {
+        this._reactionDisposer?.();
+    }
+
+
+    className(visible: boolean) {
+        return "formattedTextBox-summarizer" + (visible ? "" : "-collapsed");
+    }
+
+    onPointerDownCollapsed(e: any) {
         const visible = !this.props.node.attrs.visibility;
         const attrs = { ...this.props.node.attrs, visibility: visible };
         let textSelection = TextSelection.create(this.props.view.state.doc, this.props.getPos() + 1);
+
+
         if (!visible) { // update summarized text and save in attrs
             textSelection = this.updateSummarizedText(this.props.getPos() + 1);
             attrs.text = textSelection.content();
             attrs.textslice = attrs.text.toJSON();
         }
+
         this.props.view.dispatch(this.props.view.state.tr.
             setSelection(textSelection). // select the current summarized text (or where it will be if its collapsed)
             replaceSelection(!visible ? new Slice(Fragment.fromArray([]), 0, 0) : this.props.node.attrs.text). // collapse/expand it
             setNodeMarkup(this.props.getPos(), undefined, attrs)); // update the attrs
+
         e.preventDefault();
         e.stopPropagation();
-        const _collapsed = document.getElementById('collapse') as HTMLElement;
-        _collapsed.className = this.className(visible);
+
+        this._className = this.className(visible);
+
     }
 
     updateSummarizedText(start?: any) {
@@ -51,31 +119,23 @@ export class SummaryView extends React.Component<ISummaryView> {
         return TextSelection.create(this.props.view.state.doc, start, endPos);
     }
 
-    className = (visible: boolean) => "formattedTextBox-summarizer" + (visible ? "" : "-collapsed");
+    // @computed get fieldValueContent() {
+    //     return null;
+    // }
+    // {this.fieldValueContent}
 
-    selectNode() { }
-
-    deselectNode() { }
 
     render() {
-        const _view = this.props.node.view;
-        const js = this.props.node.toJSon;
-
-        this.props.node.toJSON = function () {
-            return js.apply(this, arguments);
-        };
-
-        const spanCollapsedClassName = this.className(this.props.node.attrs.visibility);
 
         return (
             <span
-                className={spanCollapsedClassName}
-                id='collapse'
-                onPointerDown={this.onPointerDown}
-            >
+                className={this._className}
+                onPointerDown={this.onPointerDownCollapsed}>
 
             </span>
         );
 
     }
+
 }
+
