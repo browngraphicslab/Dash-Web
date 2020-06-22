10382b7fcac2d92dc845e9b271e38de3d8c09feb
Bob Zeleznik <zzzman@gmail.com>
2020-06-21 22:29:18 -0400

    fixed link lines to text hyperlinks

--- src/client/views/collections/collectionFreeForm/CollectionFreeFormLinkView.tsx
+++ src/client/views/collections/collectionFreeForm/CollectionFreeFormLinkView.tsx
@@ -62,7 +62,7 @@ export class CollectionFreeFormLinkView extends React.Component<CollectionFreeFo
                     setTimeout(() => {
                         (this.props.A.rootDoc[(this.props.A.props as any).fieldKey] as Doc);
                         const m = targetBhyperlink.getBoundingClientRect();
-                        const mp = this.props.A.props.ContainingCollectionView?.props.ScreenToLocalTransform().transformPoint(m.right, m.top + 5);
+                        const mp = this.props.A.props.ScreenToLocalTransform().transformPoint(m.right, m.top + 5);
                         this.props.A.rootDoc[afield + "_x"] = Math.min(1, mp![0] / this.props.A.props.PanelWidth()) * 100;
                         this.props.A.rootDoc[afield + "_y"] = Math.min(1, mp![1] / this.props.A.props.PanelHeight()) * 100;
                     }, 0);
@@ -74,7 +74,7 @@ export class CollectionFreeFormLinkView extends React.Component<CollectionFreeFo
                     setTimeout(() => {
                         (this.props.B.rootDoc[(this.props.B.props as any).fieldKey] as Doc);
                         const m = targetAhyperlink.getBoundingClientRect();
-                        const mp = this.props.B.props.ContainingCollectionView?.props.ScreenToLocalTransform().transformPoint(m.right, m.top + 5);
+                        const mp = this.props.B.props.ScreenToLocalTransform().transformPoint(m.right, m.top + 5);
                         this.props.B.rootDoc[bfield + "_x"] = Math.min(1, mp![0] / this.props.B.props.PanelWidth()) * 100;
                         this.props.B.rootDoc[bfield + "_y"] = Math.min(1, mp![1] / this.props.B.props.PanelHeight()) * 100;
                     }, 0);
