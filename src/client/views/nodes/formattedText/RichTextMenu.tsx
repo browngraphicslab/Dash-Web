17b9f3672fde08864343afd9808881dd23023643
Lionel Han <47760119+IGoByJoe@users.noreply.github.com>
2020-07-01 16:47:47 -0700

    added color picker for highlighter

--- src/client/views/nodes/formattedText/RichTextMenu.tsx
+++ src/client/views/nodes/formattedText/RichTextMenu.tsx
@@ -933,7 +933,7 @@ interface ButtonDropdownProps {
 }
 
 @observer
-class ButtonDropdown extends React.Component<ButtonDropdownProps> {
+export class ButtonDropdown extends React.Component<ButtonDropdownProps> {
 
     @observable private showDropdown: boolean = false;
     private ref: HTMLDivElement | null = null;
