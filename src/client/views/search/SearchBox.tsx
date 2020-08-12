5117ba5f30afe271095ae92c511844dfe7d0cc85
bobzel <zzzman@gmail.com>
2020-08-12 11:39:55 -0400

    fixed searching on 'type' to join with parent document

--- src/client/views/search/SearchBox.tsx
+++ src/client/views/search/SearchBox.tsx
@@ -756,7 +756,7 @@ export class SearchBox extends ViewBoxBaseComponent<FieldViewProps, SearchBoxDoc
         const endIndex = 30;
         //this._endIndex = endIndex === -1 ? 12 : endIndex;
         this._endIndex = 30;
-        const headers = new Set<string>(["title", "author", "*lastModified"]);
+        const headers = new Set<string>(["title", "author", "*lastModified", "type"]);
         if ((this._numTotalResults === 0 || this._results.length === 0) && this._openNoResults) {
             if (this.noresults === "") {
                 this.noresults = "No search results :(";
