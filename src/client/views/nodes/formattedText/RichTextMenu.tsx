3a5d467707df154a4b8c4f04ab047bd1747f8d0b
Lionel Han <47760119+IGoByJoe@users.noreply.github.com>
2020-08-18 15:52:33 -0700

    fix pdf highlight change bug

--- src/client/views/nodes/formattedText/RichTextMenu.tsx
+++ src/client/views/nodes/formattedText/RichTextMenu.tsx
@@ -1005,6 +1005,7 @@ interface ButtonDropdownProps {
     dropdownContent: JSX.Element;
     openDropdownOnButton?: boolean;
     link?: boolean;
+    pdf?: boolean;
 }
 
 @observer
@@ -1044,13 +1045,22 @@ export class ButtonDropdown extends React.Component<ButtonDropdownProps> {
         }, 0);
     }
 
+
     render() {
         return (
             <div className="button-dropdown-wrapper" ref={node => this.ref = node}>
-                <div className="antimodeMenu-button dropdown-button-combined" onPointerDown={this.onDropdownClick}>
-                    {this.props.button}
-                    <div style={{ marginTop: "-8.5" }}><FontAwesomeIcon icon="caret-down" size="sm" /></div>
-                </div>
+                {!this.props.pdf ?
+                    <div className="antimodeMenu-button dropdown-button-combined" onPointerDown={this.onDropdownClick}>
+                        {this.props.button}
+                        <div style={{ marginTop: "-8.5" }}><FontAwesomeIcon icon="caret-down" size="sm" /></div>
+                    </div>
+                    :
+                    <>
+                        {this.props.button}
+                        <button className="dropdown-button antimodeMenu-button" key="antimodebutton" onPointerDown={this.onDropdownClick}>
+                            <FontAwesomeIcon icon="caret-down" size="sm" />
+                        </button>
+                    </>}
                 {this.showDropdown ? this.props.dropdownContent : (null)}
             </div>
         );
--- src/client/views/pdf/PDFMenu.tsx
+++ src/client/views/pdf/PDFMenu.tsx
@@ -112,7 +112,7 @@ export default class PDFMenu extends AntimodeMenu<AntimodeMenuProps> {
                 </div>
             </div>;
         return (
-            <ButtonDropdown key={"highlighter"} button={button} dropdownContent={dropdownContent} />
+            <ButtonDropdown key={"highlighter"} button={button} dropdownContent={dropdownContent} pdf={true} />
         );
     }
 
