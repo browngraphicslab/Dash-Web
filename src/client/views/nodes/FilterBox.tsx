e129b8b842d2a08f3835f5b6ff5ae498a2746b18
bobzel <zzzman@gmail.com>
2020-09-06 16:52:18 -0400

    fixed errors

--- src/client/views/nodes/FilterBox.tsx
+++ src/client/views/nodes/FilterBox.tsx
@@ -200,7 +200,7 @@ Scripting.addGlobal(function readFacetData(layoutDoc: Doc, facetHeader: string) 
     const set = new Set<string>();
     if (facetHeader === "tags") allCollectionDocs.forEach(child => Field.toString(child[facetHeader] as Field).split(":").forEach(key => set.add(key)));
     else allCollectionDocs.forEach(child => set.add(Field.toString(child[facetHeader] as Field)));
-    let facetValues = Array.from(set).filter(v => v);
+    const facetValues = Array.from(set).filter(v => v);
 
     let nonNumbers = 0;
     facetValues.map(val => Number.isNaN(Number(val)) && nonNumbers++);
