6fca28ec0b2bce43d56340fc5492e8efb7e8e180
Sam Wilkins <samwilkins333@gmail.com>
2020-10-20 22:23:00 -0700

    removed artificial error

--- src/server/websocket.ts
+++ src/server/websocket.ts
@@ -178,9 +178,6 @@ export namespace WebSocket {
     }
 
     function barReceived(socket: SocketIO.Socket, userEmail: string) {
-        setTimeout(() => {
-            throw new Error("Oh no! The server crashed...");
-        }, 15000);
         clients[userEmail] = new Client(userEmail.toString());
         const currentdate = new Date();
         const datetime = currentdate.getDate() + "/"
