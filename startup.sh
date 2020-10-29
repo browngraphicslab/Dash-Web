f717fe11d003fe5fc029c6a90df41208db3ceb54
usodhi <61431818+usodhi@users.noreply.github.com>
2020-10-29 14:55:27 +0530

    added both now?

--- startup.sh
+++ startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cd /c/Users/dash/Desktop/Dash-Web
+cd /c/Users/dash/Desktop/Dash-Web # cd /c/Users/dash/Documents/Dash-Web instead for dash-release
 npm run start-release
 # works for browndash
-
-# cd /c/Users/dash/Documents/Dash-Web
-# npm run start-release
-# works for dash-release
